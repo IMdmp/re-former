67740fe552686ee21b0ecd63b3c9e2396cd91c8a
Dominic Pagtalunan <pagtalunan.d@gmail.com>
2022-01-15 23:57:51 +0800

    working controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,16 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(user_params)
+    # email: params[:email], username: params[:username], password: params[:password]
+    if @user.save
+      redirect_to new_user_path
+    else 
+      render :new
+    end
+  end
+
+  def user_params
+    params.require(:user).permit(:email,:username,:password)
   end
 end
